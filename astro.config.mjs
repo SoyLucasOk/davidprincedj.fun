0ddb2d65f1822eaea6837893c467dfba4f62ef95
SoyLucasOk <soylucasok@gmail.com>
2023-04-14 18:29:57 -0300

    change icons images url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  publicDir: './public',
+  site: 'https://www.davidprincedj.fun'
+})
