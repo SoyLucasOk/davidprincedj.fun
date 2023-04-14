c325773b5b6fa669c534552686e520d8335d91d8
SoyLucasOk <soylucasok@gmail.com>
2023-04-13 23:36:21 -0300

    git init and add first files

---
+++ src/services/Links.js
@@ -0,0 +1,45 @@
+export const DP_LINKS = [
+  {
+    socialNetwork: 'Youtube',
+    url: 'https://www.youtube.com/channel/UCVHxFyQtqtMs8a3xaUCORaw',
+    icon: 'dp-youtube'
+  },
+  {
+    socialNetwork: 'Spotify',
+    url: 'https://open.spotify.com/artist/20kPVW84HyUtUbwfZL6vqK',
+    icon: 'dp-spotify'
+  },
+  {
+    socialNetwork: 'Apple Music',
+    url: 'https://music.apple.com/ar/artist/david-prince-dj/1334582670',
+    icon: 'dp-apple-music'
+  },
+  {
+    socialNetwork: 'Instagram',
+    url: 'https://www.instagram.com/davidprincedj',
+    icon: 'dp-instagram'
+  },
+  {
+    socialNetwork: 'Facebook',
+    url: 'https://www.facebook.com/Davidprincedj',
+    icon: 'dp-facebook'
+  }
+]
+
+export const DIFERENTE_RAVE_LINKS = [
+  {
+    socialNetwork: 'Youtube',
+    url: 'https://www.youtube.com/channel/UC8I424inNvtCKhMaQrftukg',
+    icon: 'dr-youtube'
+  },
+  {
+    socialNetwork: 'Instagram',
+    url: 'https://www.instagram.com/diferenterave',
+    icon: 'dr-instagram'
+  },
+  {
+    socialNetwork: 'Facebook',
+    url: 'https://www.facebook.com/DiferenteRave',
+    icon: 'dr-facebook'
+  }
+]
