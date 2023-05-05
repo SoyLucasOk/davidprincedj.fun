8bf93ce95a475a69d4289139546f00ffa38af50f
SoyLucasOk <soylucasok@gmail.com>
2023-05-05 01:25:42 -0300

    fix remainerHours

--- src/services/countdown.js
+++ src/services/countdown.js
@@ -3,7 +3,7 @@ function getRemainTime (deadline) {
   const remainTime = (new Date(deadline) - now + 1000) / 1000
   const remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2)
   const remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2)
-  const remainHours = ('0' + Math.floor(remainTime / 3600 % 60)).slice(-2)
+  const remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2)
   const remainDays = Math.floor(remainTime / (3600 * 24))
 
   return {
